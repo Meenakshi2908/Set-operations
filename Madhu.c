2a26d46bd0629360952e1b29edc39e2a917cfb93
Meenakshi2908 <86558401+Meenakshi2908@users.noreply.github.com>
2021-06-27 21:09:45 +0800

    Set operations
    This program prints the set variables and the different set operations carried out by it.

---
+++ Madhu.c
@@ -0,0 +1,6 @@
+E = {0, 27, 44, 15, 10};
+N = {11, 27, 37, 44, 54};
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
